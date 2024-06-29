1aed2886531acf15dc8a0ce52a4cb31c476281ae
Precious <chibundumozia@gmail.com>
2024-06-29 05:33:59 -0700

    easy print

--- python-import_modules/101-easy_print.py
+++ python-import_modules/101-easy_print.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-__builtins__.__dict__['_'+'_'+'im'+'port'+'__']('sys').stdout.write("#pythoniscool\n")
+__builtins__.__dict__['__import__']('sys').stdout.write("#pythoniscool\n")
