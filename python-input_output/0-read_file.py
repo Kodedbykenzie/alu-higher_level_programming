09900af754d890956895d459da89da837e31beb9
Precious <chibundumozia@gmail.com>
2024-07-16 14:13:18 -0700

    read

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+Module: 0-read_file
+Reads a text file (UTF8) and prints it to stdout.
+"""
+
+
 def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout.
+    Args:
+        filename (str): The name of the file to read.
+    """
     with open(filename, "r", encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
